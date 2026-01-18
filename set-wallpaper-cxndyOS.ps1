cac946bbd4c4262d249ffc96337e83ebc3432884
cxndy <eucxndy@gmail.com>
2026-01-18 14:24:27 -0300

    Add files via upload

--- set-wallpaper-cxndyOS.ps1
+++ set-wallpaper-cxndyOS.ps1
@@ -4,18 +4,19 @@ $wall = "C:\Windows\Web\Wallpaper\cxndyOS\cxndyOS.jpg"
 
 if (-not (Test-Path $wall)) { exit }
 
-New-Item "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization" -Force | Out-Null
-
-Set-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization" `
-    Wallpaper $wall
-
-Set-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization" `
-    WallpaperStyle 10
-
-Set-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization" `
-    LockScreenImage $wall
-
 Set-ItemProperty "HKCU:\Control Panel\Desktop" `
     Wallpaper $wall
+
+Set-ItemProperty "HKCU:\Control Panel\Desktop" `
+    WallpaperStyle 10
+
+Set-ItemProperty "HKCU:\Control Panel\Desktop" `
+    TileWallpaper 0
+
+Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Lock Screen" `
+    SlideshowEnabled 0
+
+Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Lock Screen" `
+    CreativeId "{00000000-0000-0000-0000-000000000000}"
 
 rundll32.exe user32.dll,UpdatePerUserSystemParameters
